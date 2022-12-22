98cf0f3dc55e1a7c1211e67dda516de003ec43ad
Addison <addisonrogers@protonmail.com>
2022-12-22 23:52:26 +0000

    Not sure

--- dotlove/src/routes/navbar.svelte
+++ dotlove/src/routes/navbar.svelte
@@ -3,7 +3,7 @@
 </script>
 <nav>
     <img src="static/android-chrome-192x192.png" alt="logo"/>
-    <a href="" class="text-gray-700  on:hover:text-gray-900">Home</a> //Home
+    <a href="/" class="text-gray-700  on:hover:text-gray-900">Home</a> //Home
     <a href=""> </a> //idk
     <a href=""> </a> //idk
     <a href=""> </a> //idk
